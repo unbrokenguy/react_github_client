04f46f341b40061ac7badf81acc8e5763e19202f
Bulat Khaziev <khazievbulatphanzilovich@gmail.com>
2020-10-23 16:59:37 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-function App() {
+const App: React.FunctionComponent = (): JSX.Element => {
   return (
     <div className="App">
       <h1>Hello world!</h1>
     </div>
   );
-}
+};
 
 export default App;
