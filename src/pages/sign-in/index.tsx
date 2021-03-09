f04c3ab859ff00fb6a1c629e1095c3b50293cb56
Polina <psalimullina@gmail.com>
2021-03-10 01:46:08 +0300

    add sign-in-up css

--- src/pages/sign-in/index.tsx
+++ src/pages/sign-in/index.tsx
@@ -1,28 +1,37 @@
 import React from "react";
 import "./index.scss";
+import { Link } from "react-router-dom";
 
 const SignUpPage: React.FunctionComponent = () => {
   return (
-    <div className="sign-up-page">
-      <form>
-        <div className="mb-3">
-          <label className="label">Username or email</label>
-          <input
-            type="email"
-            className="form-control form-control-lg"
-            id="InputEmail"
-          />
-          <label className="label">Password</label>
-          <input
-            type="password"
-            className="form-control form-control-lg"
-            id="InputPassword"
-          />
-          <button type="button" className="btn btn-success">
-            Sign in
-          </button>
-        </div>
-      </form>
+    <div>
+      <h1 className="header_align">Github Client</h1>
+      <div className="sign_in_page">
+        <form>
+          <div className="mb-3">
+            <label className="label">Username or email</label>
+            <input
+              type="email"
+              className="form-control form-control-lg"
+              id="InputEmail"
+            />
+            <label className="label">Password</label>
+            <input
+              type="password"
+              className="form-control form-control-lg"
+              id="InputPassword"
+            />
+            <input type="checkbox" />
+            <label className="checkbox_align">Remember me</label>
+            <Link to="/auth" className="link_align">
+              Sign in
+            </Link>
+            <button type="button" className="btn btn-success">
+              Sign in
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
--- src/pages/sign-up/index.tsx
+++ src/pages/sign-up/index.tsx
@@ -1,28 +1,35 @@
 import React from "react";
 import "./index.scss";
+import { Link } from "react-router-dom";
 
 const SignUpPage: React.FunctionComponent = () => {
   return (
-    <div className="sign-up-page">
-      <form>
-        <div className="mb-3">
-          <label className="label">Username or email</label>
-          <input
-            type="email"
-            className="form-control form-control-lg"
-            id="InputEmail"
-          />
-          <label className="label">Password</label>
-          <input
-            type="password"
-            className="form-control form-control-lg"
-            id="InputPassword"
-          />
-          <button type="button" className="btn btn-success">
-            Sign up
-          </button>
-        </div>
-      </form>
+    <div>
+      <h1 className="header_align">Github Client</h1>
+      <div className="sign_up_page">
+        <form>
+          <div className="mb-3">
+            <label className="label">Username or email</label>
+            <input
+              type="email"
+              className="form-control form-control-lg"
+              id="InputEmail"
+            />
+            <label className="label">Password</label>
+            <input
+              type="password"
+              className="form-control form-control-lg"
+              id="InputPassword"
+            />
+            <Link to="/auth" className="link_align">
+              Sign in
+            </Link>
+            <button type="button" className="btn btn-success">
+              Sign up
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
