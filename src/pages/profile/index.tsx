bbef8e183cdd113d6b0c70b273f32eccbb1ac286
Polina <psalimullina@gmail.com>
2021-03-09 13:05:21 +0300

    round-img

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -5,22 +5,31 @@ import { useQuery } from "@apollo/client";
 import Header from "../../features/header";
 import RingLoader from "react-spinners/RingLoader";
 import { query } from "../../apollo/user/query";
+import { css } from "@emotion/react";
 
 const newQuery = gql(query);
 
+const override = css`
+  position: absolute;
+  top: 30%;
+  left:40%;
+`;
+
 function Profile() {
   const { data, loading } = useQuery(newQuery);
-  console.log(loading);
   return (
     <div>
       <Header />
       {loading ? (
-        <RingLoader size={150} />
-      ) : (
+        <RingLoader size={150} css="override" />
+      ) : (P
         <div>
           <h1>UserInfo</h1>
           {data.user.login}
-          <img src={data.user.avatarUrl}></img>
+          <img
+            src={data.user.avatarUrl}
+            className="rounded-circle img_align"
+          ></img>
         </div>
       )}
     </div>
