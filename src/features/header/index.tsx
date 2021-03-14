30fe63995b1ddd58f4a912c209b287a4a9d0ac86
Polina <psalimullina@gmail.com>
2021-03-14 17:11:48 +0300

    edit header

--- src/features/header/index.tsx
+++ src/features/header/index.tsx
@@ -13,7 +13,7 @@ const Header: React.FunctionComponent = () => {
       />
       <div className="rectangle"></div>
       <h1 className="logo_header">GC</h1>
-      {<Link to="/profile" className="p_header"></Link>}
+      {<Link to="/profile" className="p_header">user</Link>}
     </div>
   );
 };
